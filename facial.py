2b9b3784d5f37c73e3efeceef03284c4f2b444c7
Olen Nasser <coldklass@gmail.com>
2025-05-26 00:00:39 -0400

    Coded the preprocessing of the images

--- facial.py
+++ facial.py
@@ -45,33 +45,61 @@ ANC_PATH = os.path.join('data', 'anchor')
 #         os.replace(EX_PATH, NEW_PATH)
         
 #Access webcamera 
+#Collect positive and anchor images (get about 300 of each)
 
-cap = cv2.VideoCapture(0)
-while cap.isOpened(): #Loops through every frame of webcam
-    ret, frame = cap.read() #Read capture at that point in time
-    frame = frame[120:120+250, 200:200+250, :] #Crop image
+# cap = cv2.VideoCapture(0)
+# while cap.isOpened(): #Loops through every frame of webcam
+#     ret, frame = cap.read() #Read capture at that point in time
+#     frame = frame[120:120+250, 200:200+250, :] #Crop image
     
 
-    #Collect Anchor Images
-    if cv2.waitKey(1) & 0xFF == ord('a'): #Waits one millisecond for pressing 'a' so it can capture image
-        imgname = os.path.join(ANC_PATH, '{}.jpg'.format(uuid.uuid1())) #Creates uniwque name for image in anchor folder
-        cv2.imwrite(imgname, frame) #Writes the image to the anchor folder
+#     #Collect Anchor Images
+#     if cv2.waitKey(1) & 0xFF == ord('a'): #Waits one millisecond for pressing 'a' so it can capture image
+#         imgname = os.path.join(ANC_PATH, '{}.jpg'.format(uuid.uuid1())) #Creates uniwque name for image in anchor folder
+#         cv2.imwrite(imgname, frame) #Writes the image to the anchor folder
 
-    #Collect Positive Images
-    if cv2.waitKey(1) & 0xFF == ord('p'): #Waits one millisecond for pressing 'p' so it can capture image
-        imgname = os.path.join(POS_PATH, '{}.jpg'.format(uuid.uuid1())) #Creates uniwque name for image in anchor folder
-        cv2.imwrite(imgname, frame) #Writes the image to the anchor folder
+#     #Collect Positive Images
+#     if cv2.waitKey(1) & 0xFF == ord('p'): #Waits one millisecond for pressing 'p' so it can capture image
+#         imgname = os.path.join(POS_PATH, '{}.jpg'.format(uuid.uuid1())) #Creates uniwque name for image in anchor folder
+#         cv2.imwrite(imgname, frame) #Writes the image to the anchor folder
 
-    cv2.imshow('Image Collection', frame) #Display the frame
+#     cv2.imshow('Image Collection', frame) #Display the frame
 
-    #Breaking gracefully
-    if cv2.waitKey(1) & 0xFF == ord('q'): #Waits one millisecond for pressing 'q' so it can close frame
-        break
-cap.release()
-cv2.destroyAllWindows()
+#     #Breaking gracefully
+#     if cv2.waitKey(1) & 0xFF == ord('q'): #Waits one millisecond for pressing 'q' so it can close frame
+#         break
+# cap.release()
+# cv2.destroyAllWindows()
+
+#Collect data
+
+anchor = tf.data.Dataset.list_files(ANC_PATH + '/*.jpg').take(300)
+positive = tf.data.Dataset.list_files(POS_PATH + '/*.jpg').take(300)
+negative = tf.data.Dataset.list_files(NEG_PATH + '/*.jpg').take(300)
+
+#Changes the values of the image to be between 0-1 instead of 0-255
+def preprocess(file_path):
+    byte_img = tf.io.read_file(file_path) #read in image from file path
+    img = tf.io.decode_jpeg(byte_img) #Load in image
+    img = tf.image.resize(img, (100, 100)) #Resizing image to be 100x100x3 (lowers quality a bit but improves runtime)
+    img = img/255.0 #changes each pixel value from 0-255 to 0-1
+    return img
+
+#Convert our data list to an array of ones and zeros
+#(anchor, positive) => 1, 1, 1, 1, 1
+#(anchor, negative) => 0, 0, 0, 0, 0
+
+positives = tf.data.Dataset.zip((anchor, positive, tf.data.Dataset.from_tensor_slices(tf.ones(len(anchor)))))
+negatives = tf.data.Dataset.zip((anchor, negative, tf.data.Dataset.from_tensor_slices(tf.zeros(len(anchor)))))
+data = positives.concatenate(negatives)
+samples = data.as_numpy_iterator()
 
 
-plt.imshow(frame) 
-plt.savefig('face.png') #Save the image
+def preprocess_twin(input_img, validation_img, label):
+    return(preprocess(input_img), preprocess(validation_img), label)
 
-
+#Dataloader pipeline
+data = data.map(preprocess_twin)
+data = data.cache()
+data = data.shuffle(buffer_size=1024)
+samples = data.as_numpy_iterator()
